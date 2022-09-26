308562d5ab201d5abfb5b02e1e043f6ba190104c
Anita Luri <anitaluri.h@gmail.com>
2022-09-26 15:15:26 -0300

    Usuario se autentica

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -14,10 +14,28 @@ describe 'Usuário se autentica' do
     end 
     #Assert
     expect(page).not_to have_link ('Entrar')
-    expect(page).to have_link 'Sair'
+    expect(page).to have_button ('Sair')
     within ('nav') do
       expect(page).to have_content 'exemplo@example.com'
     end
     expect(page).to have_content 'Login efetuado com sucesso.'
+  end
+  it 'e faz logout' do
+    #Arrange
+    User.create!(email: 'exemplo@example.com', password: 'password')
+    #Act
+    visit root_path
+    click_on 'Entrar'
+    within ('form') do
+      fill_in 'E-mail', with: 'exemplo@example.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end 
+    click_on 'Sair'
+    #Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_link 'exemplo@example.com'
   end
 end
---
+++ spec/system/user_sign_up_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Usuario se autentica' do
+  it 'com sucesso' do
+    #Arrange
+
+    #Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar uma conta'
+    fill_in 'Nome', with: 'Maria'
+    fill_in 'E-mail', with: 'test@example.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar conta'
+    #Assert
+    expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso'
+    expect(page).to have_content 'Olá Maria'
+    expect(page).to have_content 'test@example.com'
+    expect(page).to have_button 'Sair'
+  end
+end
