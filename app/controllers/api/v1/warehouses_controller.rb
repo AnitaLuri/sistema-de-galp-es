72811726917ce43abf766da645f1a60e8c85d0a6
Anita Luri <anitaluri.h@gmail.com>
2022-10-25 18:39:51 -0300

    criando galpao e tratando erros via api

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::WarehousesController < ActionController::API
+  rescue_from ActiveRecord::ActiveRecordError, with: :return_500
+  
   def show
     begin
       warehouse = Warehouse.find(params[:id])
@@ -11,5 +13,21 @@ class Api::V1::WarehousesController < ActionController::API
   def index
     warehouses = Warehouse.all
     render status: 200, json: warehouses.as_json(except: [:created_at, :updated_at])
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :state, :address, :area, 
+                                                        :cep, :description)
+    warehouse = Warehouse.new(warehouse_params)
+    if warehouse.save
+      render status: 201, json: warehouse
+    else 
+      render status: 412, json: { errors: warehouse.errors.full_messages }
+    end
+  end 
+
+  private
+  def return_500
+    render status: 500
   end
 end
--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -56,5 +56,61 @@ describe 'warehouse API' do
 
       expect(json_response).to eq []
     end
+    it 'return empy if there is no warehouse' do
+      allow(Warehouse).to receive(:all).and_raise(ActiveRecord::QueryCanceled)
+
+      get '/api/v1/warehouses'
+
+      expect(response).to have_http_status(500)
+    end 
+  end
+  context 'POST /api/v1/warehouses' do
+    it 'sucess' do
+      warehouse_params = { warehouse: { name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP',
+                                      area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                                      description: 'Galpão destinado para cargas internacionais'}
+                          }
+
+      post '/api/v1/warehouses', params: warehouse_params 
+
+      expect(response).to have_http_status(201)
+      expect(response.content_type).to include 'application/json'
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response["name"]).to eq 'Aeroporto SP'
+      expect(json_response["code"]).to eq 'GRU'
+      expect(json_response["city"]).to eq 'Guarulhos'
+      expect(json_response["state"]).to eq 'SP'
+      expect(json_response["area"]).to eq 100_000
+      expect(json_response["address"]).to eq 'Avenida do Aeroporto, 1000'
+      expect(json_response["cep"]).to eq '15000-000'
+      expect(json_response["description"]).to eq 'Galpão destinado para cargas internacionais'
+    end
+    it 'fail if parameters are not complete' do
+      warehouse_params = { warehouse: { name: 'Aeroporto SP', code: 'GRU'}
+                          }
+
+      post '/api/v1/warehouses', params: warehouse_params 
+
+      expect(response).to have_http_status(412)
+      expect(response.body).to include 'Cidade não pode ficar em branco'
+      expect(response.body).to include 'Estado não pode ficar em branco'
+      expect(response.body).to include 'Área não pode ficar em branco'
+      expect(response.body).to include 'Endereço não pode ficar em branco'
+      expect(response.body).to include 'CEP não é válido'
+      expect(response.body).to include 'Descrição não pode ficar em branco'
+    end
+    it 'fail if theres an internal error' do
+      allow(Warehouse).to receive(:new).and_raise(ActiveRecord::ActiveRecordError)
+      warehouse_params = { warehouse: { name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP',
+                                  area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                                  description: 'Galpão destinado para cargas internacionais'}
+                          }
+
+      post '/api/v1/warehouses', params: warehouse_params 
+
+      expect(response).to have_http_status(500)
+    end
   end
 end 
